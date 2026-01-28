bdc776b2cc5876ef18431e3eea75086be72d4457
Haseebdev60 <your.email@example.com>
2026-01-28 15:09:27 +0500

    Fix: Use async/await in User.ts middleware without next param to fix TS2769

--- server/src/models/User.ts
+++ server/src/models/User.ts
@@ -36,14 +36,13 @@ const userSchema = new Schema(
   { timestamps: true },
 )
 
-userSchema.pre('save', async function (next: (err?: mongoose.CallbackError) => void) {
-  if (!this.isModified('password')) return next()
+userSchema.pre('save', async function () {
+  if (!this.isModified('password')) return
   try {
     const salt = await bcrypt.genSalt(10)
     this.password = await bcrypt.hash(this.password, salt)
-    next()
-  } catch (err: any) {
-    next(err)
+  } catch (err) {
+    throw err
   }
 })
 
